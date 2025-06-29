d0ccf66a7201343ab8cf28960255dfcf8058b560
RonitKorat07 <ronitkorat7@gmail.com>
2025-06-29 10:44:20 +0530

    add video and some changes

--- server/router/apirouter.js
+++ server/router/apirouter.js
@@ -6,41 +6,41 @@ import { user_signup_controller } from '../controller/user_signup_controller.js'
 import { addToCart, getCart, removeFromCart, updateCartQuantity } from '../controller/addtocart_controller.js';
 import { saveCheckoutSummary } from '../controller/checkoutsummary_controller.js';
 import { getAllOrdersAdmin, getAllOrdersForUser, placeOrder } from '../controller/order_controller.js';
+import requiresignin from '../middleware/requiresignin.js';
 
 const router = express.Router();
 
 // categorey 
-router.post('/category/add' , addCategory)
-router.put('/category/update/:id', updateCategory);
-router.delete('/category/delete/:id', deleteCategory);
-router.get('/category' , getAllCategories)
+router.post('/category/add' , addCategory , requiresignin)
+router.put('/category/update/:id', updateCategory , requiresignin);
+router.delete('/category/delete/:id', deleteCategory , requiresignin);
+router.get('/category' , getAllCategories , requiresignin)
 
 // product 
-router.post("/product/add", createProduct);
-router.get("/product", getAllProducts);
-router.put("/product/update/:id", updateProduct);
-router.delete("/product/delete/:id", deleteProduct);
-router.get("/product/:id", getProductById);
-router.get('/products/filter', getFilteredProducts);
-router.get('/search', searchproduct  );
+router.post("/product/add", createProduct ,requiresignin);
+router.get("/product", getAllProducts ,requiresignin);
+router.put("/product/update/:id", updateProduct ,requiresignin);
+router.delete("/product/delete/:id", deleteProduct ,requiresignin);
+router.get("/product/:id", getProductById ,requiresignin);
+router.get('/products/filter', getFilteredProducts ,requiresignin);
+router.get('/search', searchproduct   ,requiresignin);
 
 //addtocart 
-router.post('/cart/add', addToCart);
-router.get("/cart/:userId", getCart); // Get user's cart
-router.delete("/cart/remove/:userId/:itemId", removeFromCart);
-router.put("/cart/update/:userId/", updateCartQuantity);
-
+router.post('/cart/add',  addToCart, requiresignin );
+router.get("/cart/:userId", getCart , requiresignin); // Get user's cart
+router.delete("/cart/remove/:userId/:itemId", removeFromCart , requiresignin);
+router.put("/cart/update/:userId/", updateCartQuantity , requiresignin);
 
 // checkoutsummry
-router.post("/cart/checkoutsummary" ,saveCheckoutSummary);
+router.post("/cart/checkoutsummary" ,saveCheckoutSummary , requiresignin);
 
 // palce order 
-router.post("/order/placeorder" ,placeOrder);
+router.post("/order/placeorder" ,placeOrder , requiresignin);
 
 //GET ALL ORDER FOR USER AND ADMIN
 
-router.get("/order/admin/all", getAllOrdersAdmin); //ADMIN
-router.get("/order/user/:userId", getAllOrdersForUser); // USER
+router.get("/order/admin/all", getAllOrdersAdmin , requiresignin); //ADMIN
+router.get("/order/user/:userId", getAllOrdersForUser , requiresignin); // USER
 
 
 
